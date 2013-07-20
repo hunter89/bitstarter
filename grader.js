fdf6efd147fb8849649c4e1f8c634ad9743bc3bf
John Smith <example@stanford.edu>
2013-07-20 11:03:25 +0530

    updated the content

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "fakeurl";
 
 var assertFileExists = function(infile){
     var instr = infile.toString();
@@ -61,13 +63,35 @@ var clone = function(fn){
 };
 
 if (require.main == module){
+    
     program
-      .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-      .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-      .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <web_url>', 'url to index.html')
+	.parse(process.argv);
+	      
+
+    if (program.url){      
+	restler.get(program.url).on('complete', function(result){
+	    if (result instanceof Error){
+		console.log(result.message);
+		 
+	    }
+	    else{
+		fs.writeFileSync('url.html', result);
+		var checkJson = checkHtmlFile('url.html', program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	    }
+	});
+    } 
+    else{
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+    
+}
+else {
     exports.checkHtmlFile = checkHtmlFile;
 } 
